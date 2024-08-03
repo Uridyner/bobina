9f6f536325ec4e4fac76364e2c1e3c9bb79727f4
Francisco José Torres Contarini <torrescontarini@gmail.com>
2024-08-03 10:46:35 -0300

    FUNCIONAAAAAAAAAAAAAAAAAA ITBA

--- bobina_v1/bobina_v1.ino
+++ bobina_v1/bobina_v1.ino
@@ -21,39 +21,41 @@ enum LadoSharp {
   SHARP_DER = 2,
 };
 /// Pines de los sharps
-constexpr uint8_t PINES_SHARPS[3] = { A7, A5, A3 };
+constexpr uint8_t PINES_SHARPS[] = { A7, A5, A3 };
 /// Numero de sharps en la placa
 constexpr size_t NUM_SHARPS = sizeof(PINES_SHARPS) / sizeof(PINES_SHARPS[0]);
+/// Distancia que usan los sharps para saber que detectan algo
+constexpr unsigned int DISTANCIA_ACTIVACION_SHARPS = 35;
 /// Cantidad de veces que se leen los sharps.
 ///
 /// Se usa para tomar un promedio de las lecturas y limpiar los valores.
-constexpr size_t LECTURAS_SHARP = 2;
+constexpr size_t LECTURAS_SHARP = 1;
 /// Lecturas por segundos de los sharp
-constexpr unsigned long FRECUENCIA_LECTURA_SHARP = 30;
+constexpr unsigned long FRECUENCIA_LECTURA_SHARP = 25;
 
 enum LadoCNY {
   CNY_IZQ = 0,
   CNY_DER = 1,
 };
 /// Pines de los CNYs
-constexpr uint8_t PINES_CNY[3] = { A7, A5, A3 };
+constexpr uint8_t PINES_CNY[] = { A6, A4 };
 /// Numero de CNYs en la placa
 constexpr size_t NUM_CNY = sizeof(PINES_CNY) / sizeof(PINES_SHARPS[0]);
-constexpr float ACTIVACIONES_CNY[NUM_CNY] = {};
+constexpr float ACTIVACIONES_CNY[NUM_CNY] = { 0.5, 0.5 };
 /// Cantidad de veces que se leen los CNYs.
 ///
 /// Se usa para tomar un promedio de las lecturas y limpiar los valores.
-constexpr size_t LECTURAS_CNY = 2;
+constexpr size_t LECTURAS_CNY = 4;
 
 /// Voltaje máximo del ADC
 constexpr float VOLTAJE_MAX_ADC = 5.0;
 /// Bits que tiene el ADC
 constexpr uint8_t BITS_ADC = 10;
 /// Valor máximo del ADC
-constexpr uint16_t VALOR_MAX_ADC = pow(2, BITS_ADC);
+constexpr uint16_t VALOR_MAX_ADC = 1024;
 /// Convertir lectura a voltaje
 constexpr float lecturaAVoltaje(uint16_t lectura) {
-  return ((float)lectura * VOLTAJE_MAX_ADC) / (float)VALOR_MAX_ADC + VOLTAJE_MAX_ADC;
+  return ((float)lectura * VOLTAJE_MAX_ADC) / (float)VALOR_MAX_ADC;
 }
 /// Convertir voltaje a lectura
 constexpr uint16_t voltajeALectura(float voltaje) {
@@ -61,19 +63,19 @@ constexpr uint16_t voltajeALectura(float voltaje) {
 }
 
 /// Pines de los LEDs
-constexpr uint8_t PINES_LEDS[] = { -1, -2 };
+constexpr uint8_t PINES_LEDS[] = { 5, 8, 4 };
 /// Numero de LEDs en la placa
 constexpr size_t NUM_LEDS = sizeof(PINES_LEDS) / sizeof(PINES_LEDS[0]);
 
 /// Pines de los botones
-constexpr uint8_t PINES_BOTONES[] = { -1, -2 };
+constexpr uint8_t PINES_BOTONES[] = { 2, 3 };
 /// Numero de botones en la placa
 constexpr size_t NUM_BOTONES = sizeof(PINES_BOTONES) / sizeof(PINES_BOTONES[0]);
 
 /// Tiempo que se espera antes de empezar a correr
 constexpr unsigned int TIEMPO_ESPERA_MS = 5000;
 
-#define DEBUG 1
+// #define DEBUG 1
 
 #if DEBUG
 #define debugSetup() Serial.begin(115200)
@@ -164,10 +166,12 @@ unsigned int distanciasSharps[NUM_SHARPS];
 unsigned long ultimaLecturaSharps = -1;
 
 unsigned int* leerSharps() {
-  if (millis() - ultimaLecturaSharps > 1000 / FRECUENCIA_LECTURA_SHARP) {
-    return;
+  if (millis() - ultimaLecturaSharps < 1000 / FRECUENCIA_LECTURA_SHARP) {
+    return distanciasSharps;
   }
+  ultimaLecturaSharps = millis();
   for (size_t i = 0; i < NUM_SHARPS; i++) {
+    distanciasSharps[i] = 0;
     for (size_t j = 0; j < LECTURAS_SHARP; j++) {
       distanciasSharps[i] += (unsigned int)round(17569.7 * pow(analogRead(PINES_SHARPS[i]), -1.2062));
     }
@@ -197,6 +201,7 @@ float lecturasCNY[NUM_CNY];
 
 float* leerCNY() {
   for (size_t i = 0; i < NUM_CNY; i++) {
+    lecturasCNY[i] = 0;
     for (size_t j = 0; j < LECTURAS_CNY; j++) {
       lecturasCNY[i] += lecturaAVoltaje(analogRead(PINES_CNY[i])) / LECTURAS_CNY;
     }
@@ -214,7 +219,7 @@ void setupCNY() {
   debugPrintln("Inicializando pines de los CNYs...");
   for (int i = 0; i < NUM_CNY; i++) {
     debugPrint("Sensor piso CNY ");
-    pinMode(PINES_SHARPS[i], INPUT);
+    pinMode(PINES_CNY[i], INPUT);
     debugPrintln(i + 1);
   }
   debugPrintln("Pines de los CNYs inicializados!");
@@ -230,19 +235,13 @@ void setupLeds() {
   debugPrintln("Pines de los leds inicializados!");
 }
 
-void cambiarLed(size_t led, bool estado) {
-  debugPrintln("Inicializando pines de los leds...");
-  for (int i = 0; i < NUM_LEDS; i++) {
-    debugPrint("Led ");
-    pinMode(PINES_LEDS[i], OUTPUT);
-    debugPrintln(i + 1);
-  }
-  debugPrintln("Pines de los leds inicializados!");
+inline void cambiarLed(size_t led, bool estado) {
+  digitalWrite(PINES_LEDS[led], estado);
 }
 
 void setupBotones() {
   debugPrintln("Inicializando pines de los botones...");
-  for (int i = 0; i < NUM_LEDS; i++) {
+  for (int i = 0; i < NUM_BOTONES; i++) {
     debugPrint("Boton ");
     pinMode(PINES_BOTONES[i], INPUT_PULLUP);
     debugPrintln(i + 1);
@@ -262,52 +261,73 @@ enum {
 void setup() {
   debugSetup();
 
-  cambiarLed(0, false);
-  cambiarLed(1, false);
+  for (size_t i = 0; i < NUM_LEDS - 1; i++) {
+    cambiarLed(i, bitRead(0, i));
+  }
   setupMotores();
 
-  cambiarLed(0, false);
-  cambiarLed(1, true);
+  for (size_t i = 0; i < NUM_LEDS - 1; i++) {
+    cambiarLed(i, bitRead(1, i));
+  }
   setupSharps();
 
-  cambiarLed(0, true);
-  cambiarLed(1, false);
+  for (size_t i = 0; i < NUM_LEDS - 1; i++) {
+    cambiarLed(i, bitRead(2, i));
+  }
   setupLeds();
 
-  cambiarLed(0, true);
-  cambiarLed(1, true);
+  for (size_t i = 0; i < NUM_LEDS - 1; i++) {
+    cambiarLed(i, bitRead(3, i));
+  }
   setupCNY();
+
+  for (size_t i = 0; i < NUM_LEDS - 1; i++) {
+    cambiarLed(i, bitRead(4, i));
+  }
+  setupBotones();
+
+  unsigned long ultimoCambioLeds = millis();
+
+  while (estaPresionado(1) == false) {
+    if (millis() - ultimoCambioLeds > 250) {
+      for (size_t i = 0; i < NUM_LEDS - 1; i++) {
+        cambiarLed(i, rand() % 2);
+      }
+      ultimoCambioLeds = millis();
+    }
+  }
+
+  unsigned long tiempoComienzo = millis();
 
 #if DEBUG
   unsigned char ultimoDigitoTiempoAnterior = -1;
 #endif
 
-  while (millis() < 5000) {
-#if DEBUG
-    const unsigned char ultimo_digito_tiempo = (TIEMPO_ESPERA_MS % 1000) / 100;
-    if (ultimo_digito_tiempo == ultimoDigitoTiempoAnterior) {
-      continue;
-    }
-    ultimoDigitoTiempoAnterior = ultimo_digito_tiempo;
-    char pluralSegundos = "s";
-    if (TIEMPO_ESPERA_MS / 1000 == 1 && ultimo_digito_tiempo == 0) {
-      pluralSegundos = 0;
-    }
-    char bufferTextoTiempoEspera[128];
-    sprintf(bufferTextoTiempoEspera, "Comenzando en %d.%1d segundo%c", TIEMPO_ESPERA_MS / 1000, ultimo_digito_tiempo);
-    debugPrintln(bufferTextoTiempoEspera);
-#endif
+  while (millis() - tiempoComienzo < TIEMPO_ESPERA_MS) {
+    unsigned long segundosRestantes = (TIEMPO_ESPERA_MS - (millis() - tiempoComienzo)) / 1000;
 
-    unsigned long tiempoLeds = millis() / 5;
-    for (size_t i = 0; i < NUM_LEDS; i++) {
-      cambiarLed(i, bitRead(tiempoLeds, i) == true);
+    for (size_t i = 0; i < NUM_LEDS - 1; i++) {
+      cambiarLed(i, bitRead(segundosRestantes, i) == true);
     }
+    cambiarLed(NUM_LEDS - 1, giroPreferido == GIRO_IZQ);
 
     if (estaPresionado(0)) {
       giroPreferido = GIRO_IZQ;
     } else if (estaPresionado(1)) {
       giroPreferido = GIRO_DER;
     }
+
+#if DEBUG
+    const unsigned char ultimoDigitoTiempo = ((TIEMPO_ESPERA_MS - (millis() - tiempoComienzo)) / 100);
+    char pluralSegundos = "s";
+    if (segundosRestantes == 1 && ultimoDigitoTiempo == 0) {
+      pluralSegundos = 0;
+    }
+    char bufferTextoTiempoEspera[128];
+    sprintf(bufferTextoTiempoEspera, "Comenzando en %d.%1d segundo%c", segundosRestantes, ultimoDigitoTiempo);
+    debugPrintln(bufferTextoTiempoEspera);
+    ultimoDigitoTiempoAnterior = ultimoDigitoTiempo;
+#endif
   }
 
   cambiarLed(0, giroPreferido == GIRO_IZQ);
@@ -324,28 +344,36 @@ unsigned long ultimoCambioRetrocediendo = -1;
 
 void loop() {
   leerCNY();
+  if (millis() - ultimoCambioRetrocediendo > 800) {
+    retrocediendo = ATRAS_NADA;
+  }
   if (retrocediendo == ATRAS_NADA) {
     if (lecturasCNY[CNY_IZQ] <= ACTIVACIONES_CNY[CNY_IZQ] && lecturasCNY[CNY_DER] <= ACTIVACIONES_CNY[CNY_DER]) {
       retrocediendo = ATRAS_RECTO;
+      ultimoCambioRetrocediendo = millis();
     } else if (lecturasCNY[CNY_IZQ] <= ACTIVACIONES_CNY[CNY_IZQ] && lecturasCNY[CNY_DER] > ACTIVACIONES_CNY[CNY_DER]) {
       retrocediendo = ATRAS_DER;
+      ultimoCambioRetrocediendo = millis();
     } else if (lecturasCNY[CNY_IZQ] > ACTIVACIONES_CNY[CNY_IZQ] && lecturasCNY[CNY_DER] <= ACTIVACIONES_CNY[CNY_DER]) {
       retrocediendo = ATRAS_IZQ;
+      ultimoCambioRetrocediendo = millis();
     }
   }
   leerSharps();
-  if (distanciasSharps[SHARP_CEN] < 60 && retrocediendo == ATRAS_NADA) {
+  if (distanciasSharps[SHARP_CEN] < DISTANCIA_ACTIVACION_SHARPS && retrocediendo == ATRAS_NADA) {
     analogWrite(MOT_L_PWM, MOT_L_PWM_MAX);
     analogWrite(MOT_R_PWM, MOT_R_PWM_MAX);
     adelante();
-  } else if (distanciasSharps[SHARP_IZQ] < 60) {
+  } else if (distanciasSharps[SHARP_IZQ] < DISTANCIA_ACTIVACION_SHARPS) {
     analogWrite(MOT_L_PWM, MOT_L_PWM_MAX);
     analogWrite(MOT_R_PWM, MOT_R_PWM_MAX);
     izquierda();
-  } else if (distanciasSharps[SHARP_DER] < 60) {
+    retrocediendo = ATRAS_NADA;
+  } else if (distanciasSharps[SHARP_DER] < DISTANCIA_ACTIVACION_SHARPS) {
     analogWrite(MOT_L_PWM, MOT_L_PWM_MAX);
     analogWrite(MOT_R_PWM, MOT_R_PWM_MAX);
     derecha();
+    retrocediendo = ATRAS_NADA;
   } else {
     switch (retrocediendo) {
       case ATRAS_RECTO:
@@ -354,28 +382,29 @@ void loop() {
         atras();
         break;
       case ATRAS_DER:
-        analogWrite(MOT_L_PWM, MOT_L_PWM_MAX);
-        analogWrite(MOT_R_PWM, MOT_R_PWM_MAX / 2);
-        atras();
-        break;
-      case ATRAS_IZQ:
-        analogWrite(MOT_L_PWM, MOT_L_PWM_MAX / 2);
+        analogWrite(MOT_L_PWM, 3 * (MOT_L_PWM_MAX / 4));
         analogWrite(MOT_R_PWM, MOT_R_PWM_MAX);
         atras();
         break;
+      case ATRAS_IZQ:
+        analogWrite(MOT_L_PWM, MOT_L_PWM_MAX);
+        analogWrite(MOT_R_PWM, 3 * (MOT_R_PWM_MAX / 4));
+        atras();
+        break;
       default:
+        analogWrite(MOT_L_PWM, MOT_L_PWM_MAX);
+        analogWrite(MOT_R_PWM, MOT_R_PWM_MAX);
         if (giroPreferido == GIRO_DER) {
-          analogWrite(MOT_L_PWM, MOT_L_PWM_MAX);
-          analogWrite(MOT_R_PWM, MOT_R_PWM_MAX);
           derecha();
         } else if (giroPreferido == GIRO_IZQ) {
-          analogWrite(MOT_L_PWM, MOT_L_PWM_MAX);
-          analogWrite(MOT_R_PWM, MOT_R_PWM_MAX);
           izquierda();
         }
     }
   }
   for (size_t i = 0; i < NUM_LEDS; i++) {
-    cambiarLed(i, bitRead(distanciasSharps[SHARP_CEN], i) == true);
+    cambiarLed(i, bitRead(distanciasSharps[SHARP_CEN] >> 2, i) == true);
   }
+#ifdef DEBUG
+  delay(100);
+#endif
 }
