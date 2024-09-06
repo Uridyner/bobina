2b3f63d43dbb638f499af7d02b52a0d0e83973d4
Francisco José Torres Contarini <torrescontarini@gmail.com>
2024-09-06 15:15:14 -0300

    Convertir tiempo que retrocede a una variable, cambiar el método de suavizamiento y hacer que use más lecturas de los sharp

--- bobina_v1/bobina_v1.ino
+++ bobina_v1/bobina_v1.ino
@@ -31,9 +31,9 @@ constexpr unsigned int DISTANCIA_ACTIVACION_SHARPS = 35;
 /// Cantidad de veces que se leen los sharps.
 ///
 /// Se usa para tomar un promedio de las lecturas y limpiar los valores.
-constexpr size_t LECTURAS_SHARP = 1;
+constexpr size_t LECTURAS_SHARP = 5;
 /// Lecturas por segundos de los sharp
-constexpr unsigned long FRECUENCIA_LECTURA_SHARP = 40;
+constexpr unsigned long FRECUENCIA_LECTURA_SHARP = 50;
 
 enum LadoCNY {
   CNY_IZQ = 0,
@@ -63,7 +63,7 @@ constexpr float lecturaAVoltaje(uint16_t lectura) {
 constexpr uint16_t voltajeALectura(float voltaje) {
   return (uint16_t)((voltaje * (float)VALOR_MAX_ADC) / VOLTAJE_MAX_ADC);
 }
-
+ 
 /// Pines de los LEDs
 constexpr uint8_t PINES_LEDS[] = { 5, 8, 4 };
 /// Numero de LEDs en la placa
@@ -76,6 +76,9 @@ constexpr size_t NUM_BOTONES = sizeof(PINES_BOTONES) / sizeof(PINES_BOTONES[0]);
 
 /// Tiempo que se espera antes de empezar a correr
 constexpr unsigned int TIEMPO_ESPERA_MS = 5000;
+
+/// Tiempo que se espera antes de empezar a correr
+constexpr unsigned int TIEMPO_RETROCEDER_MS = 600;
 
 #define DEBUG 1
 
@@ -168,21 +171,20 @@ Smoothed<unsigned int> sharps[NUM_SHARPS];
 unsigned long ultimaLecturaSharps = -1;
 
 Smoothed<unsigned int>* leerSharps() {
-  if (millis() - ultimaLecturaSharps < 1000 / FRECUENCIA_LECTURA_SHARP) {
-    return sharps;
+  if (millis() - ultimaLecturaSharps >= 1000 / FRECUENCIA_LECTURA_SHARP) {
+    ultimaLecturaSharps = millis();
+    for (size_t i = 0; i < NUM_SHARPS; i++) {
+      const unsigned int distancia = (unsigned int)round(17569.7 * pow(analogRead(PINES_SHARPS[i]), -1.2062));
+      sharps[i].add(distancia);
+      debugPrint("Sharp");
+      debugPrint(i + 1);
+      debugPrint(':');
+      debugPrint(sharps[i].get());
+      debugPrint('\t');
+    }
+    debugPrintln();
+    ultimaLecturaSharps = millis();
   }
-  ultimaLecturaSharps = millis();
-  for (size_t i = 0; i < NUM_SHARPS; i++) {
-    const unsigned int distancia = (unsigned int)round(17569.7 * pow(analogRead(PINES_SHARPS[i]), -1.2062));
-    sharps[i].add(distancia);
-    debugPrint("Sharp");
-    debugPrint(i + 1);
-    debugPrint(':');
-    debugPrint(sharps[i].get());
-    debugPrint('\t');
-  }
-  debugPrintln();
-  ultimaLecturaSharps = millis();
   return sharps;
 }
 
@@ -191,7 +193,7 @@ void setupSharps() {
   for (int i = 0; i < NUM_SHARPS; i++) {
     debugPrint("Sensor distancia sharp ");
     pinMode(PINES_SHARPS[i], INPUT);
-	  sharps[i].begin(SMOOTHED_AVERAGE, LECTURAS_SHARP);	
+    sharps[i].begin(SMOOTHED_EXPONENTIAL, LECTURAS_SHARP);
     debugPrintln(i + 1);
   }
   debugPrintln("Pines de los sharps inicializados!");
@@ -344,7 +346,7 @@ unsigned long ultimoCambioRetrocediendo = -1;
 
 void loop() {
   leerCNY();
-  if (millis() - ultimoCambioRetrocediendo > 700) {
+  if (millis() - ultimoCambioRetrocediendo > TIEMPO_RETROCEDER_MS) {
     retrocediendo = ATRAS_NADA;
   }
   if (retrocediendo == ATRAS_NADA) {
